689fea19b79d7e5df847e0b805eb4f267e921fae
oyiuu <46087741+oyiuu@users.noreply.github.com>
2021-01-22 22:05:22 +0100

    update
    added avatar command

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-const { Client } = require('discord.js');
 const Eris = require('eris');
 const config = require('./config.json');
 
-var bot = new Eris(config.token);
-bot.on("ready", () => {
-	console.log("ballin'");
-	bot.editStatus('idle', { name: 'BALLS', type: 3});
+const bot = new Eris.CommandClient(config.token, {}, {
+    prefix: "?"
 });
 
-bot.on("messageCreate", (msg) => {
-    if(msg.content === "?balls") {
-        bot.createMessage(msg.channel.id, "BALLS");
-    }
+bot.on("ready", () => {
+	console.log("ballin'");
+	bot.editStatus('idle', { name: 'with Floppa', type: 0});
 });
+
+const avatar = bot.registerCommand("avatar", (msg, args) => {
+    if(msg.author.bot) return;
+    const data = {
+        "embed": {
+          "title": "-Avatar URL-",
+          "url": msg.author.staticAvatarURL,
+          "color": 16580705,
+          "image": {
+            "url": msg.author.avatarURL
+          },
+          "author": {
+            "name": msg.author.username + "'s Avatar",
+          }
+        }
+      };
+    
+        bot.createMessage(msg.channel.id, data);
+});
+
 
 bot.connect();
