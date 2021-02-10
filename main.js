024cbf1fbaafe54834c2e1c9aabb39bf69105db3
oyiuu <46087741+oyiuu@users.noreply.github.com>
2021-02-10 16:58:06 +0100

    basic

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ client.on("message", message => {
 		client.commands.get("help").execute(message, args);
 	} else if (command === "kick") {
 		client.commands.get("kick").execute(message, args);
+	} else if (command === "ban") {
+		client.commands.get("ban").execute(message, args);
 	}
 });
 
